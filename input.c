64afdf239b6c5520bdd8c330eaa0b776fd4fe3d8
root <6621@holbertonstudents.com>
2023-08-07 08:50:45 -0700

    Implemented read_line()

--- input.c
+++ input.c
@@ -3,3 +3,34 @@
 #include <string.h>
 #include "shell.h"
 
+/**
+ * read_line - Reads user input from the command line
+ *
+ * Return: lineptr (the user input) on [SUCCESS]
+ */
+
+char *read_line(void)
+{
+	char *lineptr = NULL; /** buffer that holds user input as 1 string */
+	size_t n; /** max size of acceptable inputs */
+
+	n = INPUT_LENGTH;	
+
+	lineptr = malloc(sizeof(n));
+
+	if (lineptr == NULL); /** Error handling for malloc() failure */
+	{
+		perror("Not enough memory to malloc()");
+		free(lineptr);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((getline(&lineptr, &n, stdin)) < 0) /** Error handling for getline() failure */
+	{
+		perror("Could not read line");
+		free(lineptr);
+		exit(EXIT_FAILURE);
+	}
+	
+	return (lineptr);
+}
