e1eb8562f45c6e12ed8b35ed6a1f71066f1d1d82
allisonabinger <allisonabinger@gmail.com>
2023-08-07 13:30:35 -0700

    fixed input.c

--- input.c
+++ input.c
@@ -14,7 +14,7 @@ char *read_line(void)
 	char *lineptr = NULL; /** buffer that holds user input as 1 string */
 	size_t n; /** max size of acceptable inputs */
 
-	n = INPUT_LENGTH;	
+	n = INPUT_LENGTH;
 
 	lineptr = malloc(sizeof(n));
 
@@ -22,50 +22,45 @@ char *read_line(void)
 	{
 		printf("Not enough memory to malloc()");
 		free(lineptr);
-		return(NULL);
+		return (NULL);
 	}
 
-	if ((getline(&lineptr, &n, stdin)) < 0) /** Error handling for getline() failure */
+	if ((getline(&lineptr, &n, stdin)) < 0) /** Error handling for getline() */
 	{
 		printf("Could not read line");
 		free(lineptr);
 		return (NULL);
 	}
-	
+
 	printf("%s\n", lineptr);
 	return (lineptr);
 }
 
+/**
+ * parse_line- parses through input and returns array of tokens
+ * @line: input from main user
+ * Return: array of tokens.
+ */
 char **parse_line(char *line)
 {
 	int bufsize = 64;
 	int pos = 0;
-	char **tokens = malloc(bufsize * sizeof(char*));
+	char **tokens = malloc(bufsize * sizeof(char));
 	char *token;
-	
+
 	if (!tokens)
 	{
 		fprintf(stderr, "Allocation error\n");
 		return (NULL);
 	}
-	
+
 	token = strtok(line, delimiters);
 	while (token != NULL)
 	{
- tokens[position] = strdup(token); // Make a copy of the token
- position++;
-
- if (position >= bufsize) {
- bufsize += 64;
- tokens = realloc(tokens, bufsize * sizeof(char*));
- if (!tokens) {
- fprintf(stderr, "Allocation error\n");
- exit(EXIT_FAILURE);
- }
- }
-
- token = strtok(NULL, delimiters);
- }
- tokens[position] = NULL;
- return tokens;
+		tokens[pos] = strdup(token);
+		pos++;
+		token = strtok(NULL, delimiters);
+	}
+	tokens[pos] = NULL;
+	return (tokens);
 }
