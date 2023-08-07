c6b23a9f373d2c4ff5ab745bc816e8329c1adaee
allisonabinger <allisonabinger@gmail.com>
2023-08-07 10:06:34 -0700

    pushed parse_line

--- input.c
+++ input.c
@@ -34,3 +34,35 @@ char *read_line(void)
 	
 	return (lineptr);
 }
+
+char **parse_line(char *line)
+{
+	int bufsize = INPUT_LENGTH;
+	int position = 0;
+	char **tokens = malloc(bufsize * sizeof(char*));
+	char *token;
+	
+	if (!tokens) {
+        fprintf(stderr, "Allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, TOKEN_DELIMITERS);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += INPUT_LENGTH;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            if (!tokens) {
+                fprintf(stderr, "Allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, TOKEN_DELIMITERS);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
