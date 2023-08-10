2f9004a0b4b6797bf37742f778b753c0329b1a2a
allisonabinger <allisonabinger@gmail.com>
2023-08-10 09:43:45 -0700

    fixed exec_cmd

--- exec_cmd.c
+++ exec_cmd.c
@@ -1,25 +1,36 @@
-#include "shell.h"
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include "shell.h"
 /**
- * exec_cmd - determines if command is a built-in and executes if so
- * or calls launch_process.
- * @cmd: command passed from user
- * Return: 0 if built-in was executed, 1 if command is not built-in.
+ * exec_cmd - Determines if the command is a built-in command
+ * or calls launch_process to execute it if not.
+ * @cmd: Command passed from the user.
+ * Return: 0 if a built-in command was executed, 1 if the command is not
  */
 int exec_cmd(char **cmd)
 {
 	if (strcmp(*cmd, "cd") == 0)
 	{
-		const char *homedir;
-
-		chdir(homedir);
+		if (cmd[1] != NULL)
+		{
+			const char *homedir = cmd[1];
+			
+			if (chdir(homedir) != 0)
+			{
+				perror("cd");
+			}
+		}
+		else
+		{
+			fprintf(stderr, "cd: missing operand\n");
+		}
 		return (0);
 	}
 	else if (strcmp(*cmd, "exit") == 0)
+	{
 		exit(0);
+	}
 	else
 		return (1);
 }
