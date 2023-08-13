1ec8d54acff4876522f2507c948dc730d0e86949
allisonabinger <allisonabinger@gmail.com>
2023-08-13 11:38:30 -0700

    searched for memory allocation, included <access> in pathfinder

--- launch_process.c
+++ launch_process.c
@@ -1,40 +1,43 @@
 #include "shell.h"
-
 /**
- * launch_process - Launches a process using fork and execve
- * @cmdtoks: The command and its arguments
- *
- * Description: This function launches a new process using the fork and execve
- * system calls. The command and its arguments are passed as cmdtoks.
+ * launch_process - launches process using fork and execve
+ * @cmdtoks: command to execute
  */
 void launch_process(char **cmdtoks)
 {
-    pid_t pid;
-    int status;
+	pid_t pid;
+	int status;
 
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        return;
-    }
-    else if (pid == 0)
-    {
-        if (execve(cmdtoks[0], cmdtoks, NULL) == -1)
-        {
-            perror("execve");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        do
-        {
-            if (waitpid(pid, &status, WUNTRACED) == -1)
-            {
-                perror("waitpid");
-                return;
-            }
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return;
+	}
+	else if (pid == 0)
+	{
+		/* Check if the command has execute permissions */
+		if (access(cmdtoks[0], X_OK) == -1)
+		{
+			perror("access");
+			exit(EXIT_FAILURE);
+		}
+
+		if (execve(cmdtoks[0], cmdtoks, NULL) == -1)
+		{
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		do
+		{
+			if (waitpid(pid, &status, WUNTRACED) == -1)
+			{
+				perror("waitpid");
+				return;
+			}
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
 }
